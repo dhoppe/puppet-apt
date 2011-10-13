25afd80370be0a753089a3b5ac1c787e81ec7624
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2011-10-13 07:08:43 +0200

    Initial commit

---
+++ Modulefile
@@ -0,0 +1,9 @@
+name 'dhoppe-apt'
+version '0.0.1'
+
+author 'Dennis Hoppe'
+license 'GPLv2'
+project_page 'http://github.com/dhoppe/puppet-apt'
+source 'git://github.com/dhoppe/puppet-apt'
+summary 'APT Module'
+description 'APT Module for Debian, Ubuntu'
---
+++ manifests/init.pp
@@ -0,0 +1,101 @@
+class apt {
+	define apt::url($debian = false, $ubuntu = false, $source = false) {
+		$t_debian = $debian ? {
+			false   => "ftp.de.debian.org",
+			default => $debian,
+		}
+
+		$t_debian_src = $source ? {
+			false   => "ftp.de.debian.org",
+			default => $debian,
+		}
+
+		$t_ubuntu = $ubuntu ? {
+			false   => "de.archive.ubuntu.com",
+			default => $ubuntu,
+		}
+
+		$t_ubuntu_src = $source ? {
+			false   => "de.archive.ubuntu.com",
+			default => $ubuntu,
+		}
+
+		file { "$name":
+			owner   => root,
+			group   => root,
+			mode    => 0644,
+			notify  => Exec["aptitude-update"],
+			content => template("apt/$lsbdistcodename/etc/apt/sources.list.erb"),
+		}
+	}
+
+	exec { "aptitude-update":
+		command     => "aptitude update",
+		refreshonly => true,
+	}
+
+	if $lsbdistcodename == "lenny" {
+		file { "/etc/apt/apt.conf.d/10periodic":
+			owner  => root,
+			group  => root,
+			mode   => 0644,
+			source => "puppet:///modules/apt/$lsbdistcodename/etc/apt/apt.conf.d/10periodic",
+		}
+
+		file { "/etc/apt/apt.conf.d/20archive":
+			owner  => root,
+			group  => root,
+			mode   => 0644,
+			source => "puppet:///modules/apt/$lsbdistcodename/etc/apt/apt.conf.d/20archive",
+		}
+	} else {
+		file { "/etc/apt/apt.conf.d/10periodic":
+			owner  => root,
+			group  => root,
+			mode   => 0644,
+			source => "puppet:///modules/apt/common/etc/apt/apt.conf.d/10periodic",
+		}
+	}
+
+	if $lsbdistcodename == "lenny" {
+		file { "/etc/apt/preferences":
+			owner  => root,
+			group  => root,
+			mode   => 0644,
+			source => "puppet:///modules/apt/$lsbdistcodename/etc/apt/preferences",
+		}
+	} else {
+		file { "/etc/apt/preferences.d":
+			force  => true,
+			purge  => true,
+			owner  => root,
+			group  => root,
+			mode   => 0644,
+			source => [
+				"puppet:///modules/apt/$lsbdistcodename/etc/apt/preferences.d/$hostname",
+				"puppet:///modules/apt/$lsbdistcodename/etc/apt/preferences.d"
+			],
+		}
+	}
+
+	apt::url { "/etc/apt/sources.list":
+		debian => "192.168.122.1",
+		ubuntu => "192.168.122.1",
+		source => false,
+	}
+
+	file { "/etc/apt/sources.list.d":
+		force  => true,
+		purge  => true,
+		owner  => root,
+		group  => root,
+		mode   => 0644,
+		notify => Exec["aptitude-update"],
+		source => [
+			"puppet:///modules/apt/$lsbdistcodename/etc/apt/sources.list.d/$hostname",
+			"puppet:///modules/apt/$lsbdistcodename/etc/apt/sources.list.d"
+		],
+	}
+}
+
+# vim: tabstop=3
