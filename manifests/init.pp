26addfa1cd5ac19f97455df362e7e00bd842881e
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-04-11 13:35:27 +0200

    Fix several issues detected by puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,89 +1,89 @@
 class apt {
 	define apt::url($debian = false, $ubuntu = false, $source = false) {
 		$t_debian = $debian ? {
-			false   => "ftp.de.debian.org",
+			false   => 'ftp.de.debian.org',
 			default => $debian,
 		}
 
 		$t_debian_src = $source ? {
-			false   => "ftp.de.debian.org",
+			false   => 'ftp.de.debian.org',
 			default => $debian,
 		}
 
 		$t_ubuntu = $ubuntu ? {
-			false   => "de.archive.ubuntu.com",
+			false   => 'de.archive.ubuntu.com',
 			default => $ubuntu,
 		}
 
 		$t_ubuntu_src = $source ? {
-			false   => "de.archive.ubuntu.com",
+			false   => 'de.archive.ubuntu.com',
 			default => $ubuntu,
 		}
 
 		file { "$name":
 			owner   => root,
 			group   => root,
-			mode    => 0644,
+			mode    => '0644',
 			content => template("apt/${::lsbdistcodename}/etc/apt/sources.list.erb"),
-			notify  => Exec["aptitude-update"],
+			notify  => Exec['aptitude-update'],
 		}
 	}
 
-	exec { "aptitude-update":
-		command     => "aptitude update",
+	exec { 'aptitude-update':
+		command     => 'aptitude update',
 		refreshonly => true,
 	}
 
-	if $::lsbdistcodename == "lenny" {
-		file { "/etc/apt/apt.conf.d":
+	if $::lsbdistcodename == 'lenny' {
+		file { '/etc/apt/apt.conf.d':
 			recurse => true,
 			owner   => root,
 			group   => root,
-			mode    => 0644,
+			mode    => '0644',
 			source  => "puppet:///modules/apt/${::lsbdistcodename}/etc/apt/apt.conf.d",
 		}
 
-		file { "/etc/apt/preferences":
+		file { '/etc/apt/preferences':
 			owner  => root,
 			group  => root,
-			mode   => 0644,
+			mode   => '0644',
 			source => "puppet:///modules/apt/${::lsbdistcodename}/etc/apt/preferences",
 		}
 	} else {
-		file { "/etc/apt/apt.conf.d":
+		file { '/etc/apt/apt.conf.d':
 			recurse => true,
 			owner  => root,
 			group  => root,
-			mode   => 0644,
-			source => "puppet:///modules/apt/common/etc/apt/apt.conf.d",
+			mode   => '0644',
+			source => 'puppet:///modules/apt/common/etc/apt/apt.conf.d',
 		}
 
-		file { "/etc/apt/preferences.d":
+		file { '/etc/apt/preferences.d':
 			force   => true,
 			purge   => true,
 			recurse => true,
 			owner   => root,
 			group   => root,
-			mode    => 0644,
+			mode    => '0644',
 			source  => "puppet:///modules/apt/${::lsbdistcodename}/etc/apt/preferences.d",
 		}
 	}
 
-	apt::url { "/etc/apt/sources.list":
+	apt::url { '/etc/apt/sources.list':
 		debian => hiera('debian'),
 		ubuntu => hiera('ubuntu'),
 		source => false,
 	}
 
-	file { "/etc/apt/sources.list.d":
+	file { '/etc/apt/sources.list.d':
 		force   => true,
 		purge   => true,
 		recurse => true,
 		owner   => root,
 		group   => root,
-		mode    => 0644,
+		mode    => '0644',
 		source  => "puppet:///modules/apt/${::lsbdistcodename}/etc/apt/sources.list.d",
-		notify  => Exec["aptitude-update"],
+		notify  => Exec['aptitude-update'],
 	}
 }
 
