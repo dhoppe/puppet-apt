a22c6f15c0d8cf468fd2bc9f8adf7aa3146824d1
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-05-11 05:58:41 +0200

    Fix several issues detected by puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,99 +1,61 @@
 class apt {
-	validate_string(hiera('debian'))
-	validate_string(hiera('ubuntu'))
+  validate_string(hiera('debian'))
+  validate_string(hiera('ubuntu'))
 
-	define apt::url($debian = false, $ubuntu = false, $source = false) {
-		$t_debian = $debian ? {
-			false   => $::lsbdistcodename ? {
-				lenny   => 'archive.debian.org',
-				default => 'ftp.de.debian.org',
-			},
-			default => $debian,
-		}
+  exec { 'aptitude-update':
+    command     => 'aptitude update',
+    refreshonly => true,
+  }
 
-		$t_debian_src = $source ? {
-			false   => $::lsbdistcodename ? {
-				lenny   => 'archive.debian.org',
-				default => 'ftp.de.debian.org',
-			},
-			default => $debian,
-		}
+  if $::lsbdistcodename == 'lenny' {
+    file { '/etc/apt/apt.conf.d':
+      recurse => true,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      source  => "puppet:///modules/apt/${::lsbdistcodename}/etc/apt/apt.conf.d",
+    }
 
-		$t_ubuntu = $ubuntu ? {
-			false   => 'de.archive.ubuntu.com',
-			default => $ubuntu,
-		}
+    file { '/etc/apt/preferences':
+      owner  => root,
+      group  => root,
+      mode   => '0644',
+      source => "puppet:///modules/apt/${::lsbdistcodename}/etc/apt/preferences",
+    }
+  } else {
+    file { '/etc/apt/apt.conf.d':
+      recurse => true,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      source  => 'puppet:///modules/apt/common/etc/apt/apt.conf.d',
+    }
 
-		$t_ubuntu_src = $source ? {
-			false   => 'de.archive.ubuntu.com',
-			default => $ubuntu,
-		}
+    file { '/etc/apt/preferences.d':
+      force   => true,
+      purge   => true,
+      recurse => true,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      source  => "puppet:///modules/apt/${::lsbdistcodename}/etc/apt/preferences.d",
+    }
+  }
 
-		file { "$name":
-			owner   => root,
-			group   => root,
-			mode    => '0644',
-			notify  => Exec['aptitude-update'],
-			content => template("apt/${::lsbdistcodename}/etc/apt/sources.list.erb"),
-		}
-	}
+  apt::url { '/etc/apt/sources.list':
+    debian => hiera('debian'),
+    ubuntu => hiera('ubuntu'),
+    source => false,
+  }
 
-	exec { 'aptitude-update':
-		command     => 'aptitude update',
-		refreshonly => true,
-	}
-
-	if $::lsbdistcodename == 'lenny' {
-		file { '/etc/apt/apt.conf.d':
-			recurse => true,
-			owner   => root,
-			group   => root,
-			mode    => '0644',
-			source  => "puppet:///modules/apt/${::lsbdistcodename}/etc/apt/apt.conf.d",
-		}
-
-		file { '/etc/apt/preferences':
-			owner  => root,
-			group  => root,
-			mode   => '0644',
-			source => "puppet:///modules/apt/${::lsbdistcodename}/etc/apt/preferences",
-		}
-	} else {
-		file { '/etc/apt/apt.conf.d':
-			recurse => true,
-			owner  => root,
-			group  => root,
-			mode   => '0644',
-			source => 'puppet:///modules/apt/common/etc/apt/apt.conf.d',
-		}
-
-		file { '/etc/apt/preferences.d':
-			force   => true,
-			purge   => true,
-			recurse => true,
-			owner   => root,
-			group   => root,
-			mode    => '0644',
-			source  => "puppet:///modules/apt/${::lsbdistcodename}/etc/apt/preferences.d",
-		}
-	}
-
-	apt::url { '/etc/apt/sources.list':
-		debian => hiera('debian'),
-		ubuntu => hiera('ubuntu'),
-		source => false,
-	}
-
-	file { '/etc/apt/sources.list.d':
-		force   => true,
-		purge   => true,
-		recurse => true,
-		owner   => root,
-		group   => root,
-		mode    => '0644',
-		notify  => Exec['aptitude-update'],
-		source  => "puppet:///modules/apt/${::lsbdistcodename}/etc/apt/sources.list.d",
-	}
+  file { '/etc/apt/sources.list.d':
+    force   => true,
+    purge   => true,
+    recurse => true,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    notify  => Exec['aptitude-update'],
+    source  => "puppet:///modules/apt/${::lsbdistcodename}/etc/apt/sources.list.d",
+  }
 }
-
-# vim: tabstop=3
